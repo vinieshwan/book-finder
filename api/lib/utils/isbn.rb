336721b8430a47bcc8d5d17030d0bb6b9fa62b3d
Vinia Soldia <vinia.chriszon.soldia@gmail.com>
2023-04-22 08:17:49 +0800

    Create a method to determine the grouping of isbn

--- api/lib/utils/isbn.rb
+++ api/lib/utils/isbn.rb
@@ -3,6 +3,7 @@
 module Utils
   # Handles conversion
   class Isbn
+    # rubocop: todo Metrics/ClassLength
     class << self
       def isbn_10?(isbn)
         return false if /[A-WYZa-z]+/ =~ isbn
@@ -40,7 +41,7 @@ module Utils
 
         number.push(checkdigit == 10 ? 'X' : checkdigit)
 
-        number.join.insert(1, '-').insert(5, '-').insert(11, '-')
+        hyphenate_isbn(number.join)
       end
 
       def to_isbn13(isbn)
@@ -51,10 +52,110 @@ module Utils
 
         number.push(checkdigit == 10 ? 0 : checkdigit)
 
-        number.join.insert(3, '-').insert(5, '-').insert(11, '-').insert(15, '-')
+        hyphenate_isbn(number.join)
       end
 
       private
+
+      # rubocop: todo Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
+      def hyphenate_isbn(isbn)
+        prefix = isbn.length == 13 ? "#{isbn[0...3]}-" : ''
+        group = isbn.length == 13 ? isbn[3..] : isbn
+
+        rg = group[0...1].to_i
+        re = group[1...3].to_i
+        pe = group[1...5].to_i
+
+        case rg
+        when 0, 3, 4
+          case re
+          when 0...20
+            regex = /(\d)(\d{2})(\d{6})(\w)/
+          when 0...70
+            regex = /(\d)(\d{3})(\d{5})(\w)/
+          when 0...85
+            regex = /(\d)(\d{4})(\d{4})(\w)/
+          when 0...90
+            regex = /(\d)(\d{5})(\d{3})(\w)/
+          when 0...95
+            regex = /(\d)(\d{6})(\d{2})(\w)/
+          when 0..99
+            regex = /(\d)(\d{7})(\d)(\w)/
+          end
+        when 1
+          case pe
+          when 0...1000
+            regex = /(\d)(\d{2})(\d{6})(\w)/
+          when 0...4000
+            regex = /(\d)(\d{3})(\d{5})(\w)/
+          when 0...5500
+            regex = /(\d)(\d{4})(\d{4})(\w)/
+          when 0...8698
+            regex = /(\d)(\d{5})(\d{3})(\w)/
+          when 0...9990
+            regex = /(\d)(\d{6})(\d{2})(\w)/
+          when d4..9999
+            regex = /(\d)(\d{7})(\d)(\w)/
+          end
+        when 2
+          case re
+          when 0...20
+            regex = /(\d)(\d{2})(\d{6})(\w)/
+          when 0...70
+            regex = /(\d)(\d{3})(\d{5})(\w)/
+          when 0...84
+            regex = /(\d)(\d{4})(\d{4})(\w)/
+          when 0...90
+            regex = /(\d)(\d{5})(\d{3})(\w)/
+          when 0...95
+            regex = /(\d)(\d{6})(\d{2})(\w)/
+          when 0..99
+            regex = /(\d)(\d{7})(\d)(\w)/
+          end
+        when 9
+          d2 = group[2...4] if group[1...2].zero?
+
+          case d2
+          when 0...20
+            regex = /(\d{2})(\d{2})(\d{5})(\w)/
+          when 0...50
+            regex = /(\d{2})(\d{3})(\d{4})(\w)/
+          when 0...70
+            regex = /(\d{2})(\d{4})(\d{3})(\w)/
+          when 0...80
+            regex = /(\d{2})(\d{5})(\d{2})(\w)/
+          when 0..81
+            regex = /(\d{2})(\d{6})(\d)(\w)/
+          end
+
+          d2 = group[3...5] if group[1...3] == 65
+
+          case d2
+          when 0...20
+            regex = /(\d{3})(\d{2})(\d{4})(\w)/
+          when 0...70
+            regex = /(\d{3})(\d{3})(\d{3})(\w)/
+          when 0...90
+            regex = /(\d{3})(\d{4})(\d{2})(\w)/
+          when 0..95
+            regex = /(\d{3})(\d{5})(\d)(\w)/
+          end
+
+          d2 = group[3...5] if group[1...3] == 81
+
+          case d2
+          when 0...20
+            regex = /(\d{3})(\d{2})(\d{4})(\w)/
+          when 0...30
+            regex = /(\d{3})(\d{3})(\d{3})(\w)/
+          when 0..40
+            regex = /(\d{3})(\d{4})(\d{2})(\w)/
+          end
+        end
+
+        "#{prefix}#{group.gsub(regex, '\1-\2-\3-\4')}"
+      end
+      # rubocop: enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
 
       def isbn_cleanup(isbn)
         isbn.gsub(/[-\s]*/, '').split('')
@@ -80,5 +181,6 @@ module Utils
         sum % 10
       end
     end
+    # rubocop: enable Metrics/ClassLength
   end
 end
